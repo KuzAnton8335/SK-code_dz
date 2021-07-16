32ac601c00a2625fc28167c81029ab88a77d2ce9
KuzAnton8335 <ant2783@gmail.com>
2021-07-16 10:01:04 +0300

    base-custom

---
+++ base-custom/js/script.js
@@ -0,0 +1,166 @@
+// // Pass single element
+// const element = document.querySelector("select");
+// const choices = new Choices(element, {
+//   searchEnabled: false,
+//   silent: false,
+// });
+
+//map
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+ymaps.ready(init);
+function init() {
+  // Создание карты.
+  var myMap = new ymaps.Map("myMap", {
+    // Координаты центра карты.
+    // Порядок по умолчанию: «широта, долгота».
+    // Чтобы не определять координаты центра карты вручную,
+    // воспользуйтесь инструментом Определение координат.
+    center: [48.87219215110793, 2.3542561865081497],
+    // Уровень масштабирования. Допустимые значения:
+    // от 0 (весь мир) до 19.
+    zoom: 18,
+  });
+
+  // Создание геообъекта с типом точка (метка).
+  //   var myGeoObject = new ymaps.GeoObject({
+  //     geometry: {
+  //       type: "Point", // тип геометрии - точка
+  //       coordinates: [55.8, 37.8], // координаты точки
+  //     },
+  //   });
+
+  var myPlacemark = new ymaps.Placemark(
+    [48.87219215110793, 2.3542561865081497],
+    {},
+    {
+      iconLayout: "default#image",
+      iconImageHref: "img/location-pin.svg",
+      iconImageSize: [48, 48],
+      iconImageOffset: [-34, -62],
+    }
+  );
+
+  // Размещение геообъекта на карте.
+  //   myMap.geoObjects.add(myGeoObject);
+  myMap.geoObjects.add(myPlacemark);
+}
+
+//mask
+
+var selector = document.querySelector("input[type='tel']");
+var im = new Inputmask("+7(999)-999-99-99");
+
+im.mask(selector);
+
+new JustValidate(".form", {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 30,
+    },
+    tel: {
+      required: true,
+      function: (name, value) => {
+        const phone = selector.inputmask.unmaskedvalue();
+        return Number(phone) && phone.length === 10;
+      },
+    },
+    mail: {
+      required: true,
+      email: true,
+    },
+  },
+});
+
+// select
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /*for each element, create a new DIV that will act as the selected item:*/
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function (e) {
+      /*when an item is clicked, update the original select box,
+        and the selected item:*/
+      var y, i, k, s, h, sl, yl;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      sl = s.length;
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < sl; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          yl = y.length;
+          for (k = 0; k < yl; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+      h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function (e) {
+    /*when the select box is clicked, close any other select boxes,
+      and open/close the current select box:*/
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x,
+    y,
+    i,
+    xl,
+    yl,
+    arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i);
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
+
+tippy("#myButton", {
+  content: "Глава 2, страница 176",
+});
